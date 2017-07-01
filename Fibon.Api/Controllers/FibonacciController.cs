541e379aa49623be0ed92899322d7ddd2bf60037
adamsow <adam_sowinski@gazeta.pl>
2017-07-01 10:48:14 +0200

    RabbitMQ added

--- Fibon.Api/Controllers/FibonacciController.cs
+++ Fibon.Api/Controllers/FibonacciController.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
 
 namespace Fibon.Api.Controllers
 {
     [Route("[controller]")]
     public class FibonacciController : Controller
     {
+        private readonly IBusClient _busCient;
+        public FibonacciController(IBusClient busClient)
+        {
+            _busCient = busClient;
+        }
         [HttpGet("{number}")]
         public IActionResult Get(int number)
         {
